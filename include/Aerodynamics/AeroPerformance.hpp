14878529d62355d8101de8c3a9e22388f66c8e97
Alex Roup <alex@avraero.com>
2025-11-07 07:41:01 -0500

    moved aero stuff to AeroPerformance

--- include/Aerodynamics/AeroPerformance.hpp
+++ include/Aerodynamics/AeroPerformance.hpp
@@ -1,3 +1,7 @@
+#pragma once
+
+#include "Airframe/AirframePerformance.hpp"
+
 // Aerodynamic performance model for the aircraft
 // determines load factor and roll rate capabilities at each iteration
 
@@ -6,8 +10,35 @@ class AeroPerformance {
     public:
 
         float CLmax; // maximum lift coefficient in the wind Z axis
+        float CLmin; // minimum lift coefficient in the wind Z axis
         float Cmxmax; // maximum roll moment coefficient in the wind X axis
         float Cmymax; // maximum pitch moment coefficient in the wind Y axis
 
-};
+        float Sref; // reference area in m^2
+        float bref; // wingspan in m
+        float cref; // mean aerodynamic chord in m
+        
+        float E; // Oswald efficiency factor
+        float CD0; // zero-lift drag coefficient
 
+        float AR() const;
+        float K() const;
+        float CDi(float CL) const;
+        float CL(float V, float rho, float N, float mass_kg, AirframePerformance &airframe) const;
+        float CLalpha(float rho, float V) const;
+        float beta(float rho, float V) const;
+        float q(float V, float rho) const;
+        float CD(float CL) const;
+
+        float Drag(float V, float rho, float N, float mass_kg, AirframePerformance &airframe) const;
+        float Lift(float V, float rho, float N, float mass_kg, AirframePerformance &airframe) const;
+
+        float EAS(float TAS, float rho) const;
+        float EAS2TAS(float EAS, float rho) const;
+        float Mach(float TAS) const;
+
+        static const float rhoSL;
+        static const float a_Mach; // speed of sound at sea level in m/s
+        static const float g; // standard gravity in m/s^2
+
+};
---
+++ src/Aerodynamics/AeroPerformance.cpp
@@ -0,0 +1,72 @@
+#define _USE_MATH_DEFINES
+
+#include "Aerodynamics/AeroPerformance.hpp"
+#include <cmath>
+#include "Math/math_util.hpp"
+
+const float AeroPerformance::rhoSL = 1.225f; // sea level standard density in kg/m^3
+const float AeroPerformance::a_Mach = 340.29f; // speed of sound at sea level in m/s
+const float AeroPerformance::g = 9.81f; // standard gravity in m/s^2
+
+
+float AeroPerformance::AR() const {
+    return (bref * bref) / Sref;
+}
+
+float AeroPerformance::K() const {
+    return 1.0f / (M_PI * AR() * E);
+}
+
+float AeroPerformance::CDi(float CL) const {
+    return K() * CL * CL;
+}
+
+float AeroPerformance::q(float V, float rho) const {
+    return 0.5f * rho * V * V;
+}
+
+float AeroPerformance::CL(float V, float rho, float N, float mass_kg, AirframePerformance &airframe) const
+{
+    float L = MathUtil::clip(N, airframe.GMin, airframe.GMax) * mass_kg * g;
+    return MathUtil::clip(L / (q(V, rho) * Sref), CLmin, CLmax);
+}
+
+// CD
+float AeroPerformance::CD(float CL) const
+{
+    return CD0 + CDi(CL);
+}
+
+// this is a total drag
+float AeroPerformance::Drag(float V, float rho, float N, float mass_kg, AirframePerformance &airframe) const
+{
+    float i_CD = CD(CL(V,rho,N,mass_kg,airframe));
+
+    return i_CD * q(V,rho) * Sref;
+}
+
+float AeroPerformance::Lift(float V, float rho, float N, float mass_kg, AirframePerformance &airframe) const
+{
+    return CL(V,rho,N,mass_kg,airframe) * q(V,rho) * Sref;
+}
+
+// TODO: check this
+float AeroPerformance::CLalpha(float rho, float V) const {
+    return (2.0f * M_PI * AR()) / (2.0f + sqrt(4.0f + pow((AR() * beta(rho, V) / E), 2.0f))) * (Sref / q(V,rho));
+}
+
+float AeroPerformance::beta(float rho, float V) const {
+    return sqrt(1 - Mach(V) * Mach(V));
+}
+
+float AeroPerformance::EAS(float TAS, float rho) const {
+    return TAS * sqrt(rhoSL / rhoSL);
+}
+
+float AeroPerformance::EAS2TAS(float EAS, float rho) const {
+    return EAS / sqrt(rhoSL / rhoSL);
+}
+
+float AeroPerformance::Mach(float TAS) const {
+    return TAS / a_Mach;
+}
