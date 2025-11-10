195b5c7e85a9f332f197d8123b160bf3ce1eab22
Alex Roup <alex@avraero.com>
2025-11-10 12:21:21 -0500

    nDim -> order, formatting

--- include/Control/SISOFilter.hpp
+++ include/Control/SISOFilter.hpp
@@ -9,64 +9,62 @@
 // with ARMA parameterization
 // NOTE: filter parameterization enforces finite DC gain
 // template <char NUM_STATES=FILTER_MAX_STATES>
-class SISOFilter : SISOBlock {
+class SISOFilter : SISOBlock
+{
 
-    public:
+public:
+    SISOFilter()
+    {
+        num << 1;
+        den << 1;
+        uBuff << 0;
+        yBuff << 0;
+    }
 
-        SISOFilter()
-        {
-            num << 1;
-            den << 1;
-            uBuff << 0;
-            yBuff << 0;
-        }
+    SISOFilter(SISOFilter &filt)
+    {
+        copy(filt);
+    }
 
-        SISOFilter(SISOFilter &filt)
-        {
-            copy(filt);
-        }
+    void copy(SISOFilter &filt);
 
-        void copy(SISOFilter &filt);
+    // IIR filter design
+    void setLowPassFirstIIR(float dt, float tau);                  // first order low pass filter design
+    void setLowPassSecondIIR(float dt, float wn_rps, float zeta);  // second order low pass filter design
+    void setHighPassFirstIIR(float dt, float tau);                 // first order high pass filter design
+    void setHighPassSecondIIR(float dt, float wn_rps, float zeta); // second order high pass filter design
+    void setDerivIIR(float dt, float tau);                         // first order derivative + low pass filter design
+    void setNotchSecondIIR(float dt, float wn_rps, float zeta);    // second order notch filter design
 
-        // IIR filter design
-        void setLowPassFirstIIR(float dt, float tau); // first order low pass filter design
-        void setLowPassSecondIIR(float dt, float wn_rps, float zeta); // second order low pass filter design
-        void setHighPassFirstIIR(float dt, float tau); // first order high pass filter design
-        void setHighPassSecondIIR(float dt, float wn_rps, float zeta); // second order high pass filter design
-        void setDerivIIR(float dt, float tau);  // first order derivative + low pass filter design
-        void setNotchSecondIIR(float dt, float wn_rps, float zeta);  // second order notch filter design
+    char order() { return den.rows() - 1; }
 
-        char nDim() { return den.rows() -1; }
+    // FIR filter design
+    void setButterworthFIR(char order);    // Butterworth low pass FIR filter design
+    void setAverageFIR(char order);        // equal weight moving average FIR filter design
+    void setExpFIR(char order, float tau); // exponential decaying weight moving average FIR filter design
 
-        // FIR filter design
-        void setButterworthFIR(char dim); // Butterworth low pass FIR filter design
-        void setAverageFIR(char dim); // equal weight moving average FIR filter design
-        void setExpFIR(char dim, float tau); // exponential decaying weight moving average FIR filter design
+    // step the filter
+    float step(float in);
 
-        // step the filter
-        float step(float in);  
+    // reset the fiter based on inputs
+    void reset_input(float in);
 
-        // reset the fiter based on inputs
-        void reset_input(float in);
+    // Reset the filter based on outputs
+    // If dc gain is zero, then the filter is
+    // reset to zero regardless of argument value
+    void reset_output(float out);
 
-        // Reset the filter based on outputs
-        // If dc gain is zero, then the filter is
-        // reset to zero regardless of argument value
-        void reset_output(float out);
+    // dc gain value of the filter
+    float dc_gain();
 
-        // dc gain value of the filter
-        float dc_gain();
+private:
+    Eigen::Matrix<float, Eigen::Dynamic, 1, 0, NUM_STATES + 1, 1> num;
+    Eigen::Matrix<float, Eigen::Dynamic, 1, 0, NUM_STATES + 1, 1> den;
 
-    private:
+    Eigen::Matrix<float, Eigen::Dynamic, 1, 0, NUM_STATES + 1, 1> uBuff;
+    Eigen::Matrix<float, Eigen::Dynamic, 1, 0, NUM_STATES + 1, 1> yBuff;
 
-        Eigen::Matrix<float, Eigen::Dynamic, 1, 0, NUM_STATES+1, 1> num;
-        Eigen::Matrix<float, Eigen::Dynamic, 1, 0, NUM_STATES+1, 1> den;
-
-        Eigen::Matrix<float, Eigen::Dynamic, 1, 0, NUM_STATES+1, 1> uBuff;
-        Eigen::Matrix<float, Eigen::Dynamic, 1, 0, NUM_STATES+1, 1> yBuff;
-
-        // Tustin discrete IIR filter realization
-        void tustin_first();
-        void tustin_second();
-
+    // Tustin discrete IIR filter realization
+    void tustin_first();
+    void tustin_second();
 };
