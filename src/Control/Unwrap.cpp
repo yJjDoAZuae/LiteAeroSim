395f4bb7d1df9ae10574b9e34e6728f33af6e02b
Alex Roup <alex@avraero.com>
2025-11-19 06:47:39 -0500

    unwrapper class

---
+++ src/Control/Unwrap.cpp
@@ -0,0 +1,20 @@
+#include "Control/Unwrap.hpp"
+#include "Math/math_util.hpp"
+
+using namespace Control;
+
+float Unwrap::step(float u) 
+{
+    _in = u;
+    _out += MathUtil::wrapToPi(_in - _out);;
+
+    return _out;
+}
+
+float Unwrap::step(float u, float ref) 
+{
+    _in = u;
+    _out += MathUtil::wrapToPi(_in - ref);;
+
+    return _out;
+}
