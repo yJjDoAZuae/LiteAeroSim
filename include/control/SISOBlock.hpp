425afa68dfa605abb02f00f08d2e3401893884a9
Alex Roup <alex@avraero.com>
2025-12-20 08:08:52 -0500

    cleanup

--- include/control/SISOBlock.hpp
+++ include/control/SISOBlock.hpp
@@ -4,13 +4,12 @@ class SISOBlock {
 
     public:
 
-        virtual ~SISOBlock() {};
+        SISOBlock() {}
+        virtual ~SISOBlock() {}
 
         virtual float in() const = 0;
         virtual float out() const = 0;
         virtual operator float() const = 0;
-
-        SISOBlock() {}
 
         // iteration interface
         virtual float step(float u)=0;
